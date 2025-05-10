c81248612e921fcadbaaa0b05a1bbc20f8284071
tvaditya <tvaditya@gmail.com>
2025-05-10 16:41:44 -0300

    Bugs, bugs and bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "background-primary": "#050505",
+                "background-secondary": "#0F0F10",
+                "background-tertiary": "#19191A",
+                "content-body": "#CDCBCC",
+                "content-placeholder": "#827D7F",
+                "content-headline": "#B2B2B2",
+                "border-primary": "#19191A",
+                "border-secondary": "#323234",
+                "border-tertiary": "#97979B",
+                "accent-purple": "#4B2DBB",
+                "accent-green": "#87BB2D",
+                "accent-pink": "#B5446B"
+            },
+        },
+    },
+    plugins: [],
 }
-
