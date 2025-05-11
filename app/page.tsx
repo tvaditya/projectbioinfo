8b0f51691e7b2cb9b427a585bda7264edd1ee8a8
tvaditya <tvaditya@gmail.com>
2025-05-11 17:15:53 -0300

    FAQ criada

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import FAQ from "./components/landing-page/faq";
 import Header from "./components/landing-page/header";
 import Hero from "./components/landing-page/hero";
 import Pricing from "./components/landing-page/pricing";
@@ -10,8 +11,7 @@ export default function Home() {
             <Hero />
             <VideoExplanation />
             <Pricing />
-
-            {/* <FAQ /> */}
+            <FAQ />
         </div>
     );
 }
