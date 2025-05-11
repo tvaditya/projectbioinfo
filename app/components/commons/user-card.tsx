4b647647810e4588a17868a3fca930f58767a7ab
tvaditya <tvaditya@gmail.com>
2025-05-11 15:52:02 -0300

    product card created

--- app/components/commons/user-card.tsx
+++ app/components/commons/user-card.tsx
@@ -8,15 +8,15 @@ export default function UserCard() {
         <div className="w-[348px] flex flex-col gap-5 items-center p-5 border border-white border-opacity-10 bg-[#121212] rounded-3xl text-white">
             <div className="size-48">
                 <img
-                    src="/me.webp"
-                    alt="André Dev"
+                    src="/me.jpg"
+                    alt="Adi Dev"
                     className="rounded-full object-cover w-full h-full"
                 />
             </div>
             <div className="flex flex-col gap-2 w-full">
                 <div className="flex items-center gap-2">
                     <h3 className="text-3xl font-bold min-w-0 overflow-hidden">
-                        André Dev
+                        Adi Dev
                     </h3>
                 </div>
                 <p className="opacity-40">"Eu faço produtos para a Internet"</p>
