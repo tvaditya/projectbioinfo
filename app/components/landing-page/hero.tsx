68ac1d1ac6944a9ec0c38e3a65bd2d1fec5e4549
tvaditya <tvaditya@gmail.com>
2025-05-11 15:21:49 -0300

    Total visits created

--- app/components/landing-page/hero.tsx
+++ app/components/landing-page/hero.tsx
@@ -1,3 +1,4 @@
+import TotalVisits from "../commons/total-visits";
 import UserCard from "../commons/user-card";
 import Button from "../ui/button";
 import TextInput from "../ui/text-input";
@@ -25,7 +26,7 @@ export default function Hero() {
                 <div className="relative">
                     <UserCard />
                     <div className="absolute -bottom-[7%] -right-[45%]">
-                        {/* <TotalVisits /> */}
+                        <TotalVisits />
                     </div>
                     <div className="absolute top-[20%] -left-[45%] -z-10">
                         {/* <ProjectCard /> */}
